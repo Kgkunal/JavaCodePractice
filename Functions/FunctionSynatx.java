d3ea80fd73325e4ed5039249836b50b9d35a3875
Kunal D Gavhane <kgkunalgavhane1721@gmail.com>
2023-09-20 06:50:10 +0530

    patterns done

---
+++ Functions/FunctionSynatx.java
@@ -0,0 +1,5 @@
+public class FunctionSynatx {
+    public static void main(String []args){
+        System.out.println("Syntax for functions");
+   }
+}
---
+++ Patterns/More advance patterns/SquarePattern.java
@@ -0,0 +1,8 @@
+public class SquarePattern {
+    public static void main(String []args){
+        System.out.println("Print Square Pattern");
+        for(int i=1 ; i<=4 ; i++){
+            for
+        }
+    }
+}
