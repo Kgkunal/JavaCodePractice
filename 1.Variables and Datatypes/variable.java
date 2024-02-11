ee7ca79ceddb418cbfb2822a75945cd2255b9e10
Kunal D Gavhane <kgkunalgavhane1721@gmail.com>
2024-02-11 16:24:17 +0530

    Revising some topics

---
+++ 1.Variables and Datatypes/variable.java
@@ -0,0 +1,10 @@
+public class variable {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 10;
+        // System.out.println(a);
+
+        a = 10;
+        System.out.println(a);
+    }
+}
